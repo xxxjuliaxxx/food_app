de834af1304aec0021fef4becd9d2c6457629f5b
Julia Czaplejewicz <czaplejewiczjulia@gmail.com>
2023-01-19 16:40:58 +0100

    Add slider

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -13,6 +13,7 @@ class AddOpinionPageContent extends StatefulWidget {
 class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
   var restaurantName = '';
   var pizzaName = '';
+  var rating = 3.0;
 
   @override
   Widget build(BuildContext context) {
@@ -40,12 +41,24 @@ class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
               });
             },
           ),
+          Slider(
+            onChanged: (newValue) {
+              setState(() {
+                rating = newValue;
+              });
+            },
+            value: rating,
+            min: 1.0,
+            max: 5.0,
+            divisions: 10,
+            label: rating.toString(),
+          ),
           ElevatedButton(
             onPressed: () {
               FirebaseFirestore.instance.collection('restaurants').add({
                 'name': restaurantName,
                 'pizza': pizzaName,
-                'rating': 3.0,
+                'rating': rating,
               });
             },
             child: const Text('Add'),
